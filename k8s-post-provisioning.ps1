0d684262604e11aefa848b33bb1c796a93e144b0
Litewka <mateusz.litewka@capgemini.com>
2022-04-20 14:10:06 +0200

    t2007

--- k8s-post-provisioning.ps1
+++ k8s-post-provisioning.ps1
@@ -25,4 +25,4 @@ foreach ($namespace in $aksNamespacesArray) {
 }
 
 $aksClusterObject | Invoke-AzAksRunCommand -Command "kubectl create serviceaccount sa-cicd --namespace cicd" -Force
-$aksClusterObject | Invoke-AzAksRunCommand -CommandContextAttachment "./custom-roles.yaml" -Command "ls -al && whoami" -Force
+$aksClusterObject | Invoke-AzAksRunCommand -CommandContextAttachment "./custom-roles.yaml" -Command "ls -al && whoami && chmod 777 custom-roles.yaml && ls -al" -Force
